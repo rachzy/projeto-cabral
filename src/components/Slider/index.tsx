95bd44fdefb5131a64a9822646efd2678b965ed4
Henrique <henriqueferreira.fox@gmail.com>
2023-04-27 15:39:19 -0300

    create slider

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./Slider.css";
+
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+interface IProps {
+  images: string[];
+}
+
+const Slider: React.FC<IProps> = ({ images }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  function handleNextSlideButtonClick() {
+    setCurrentSlide((currentValue) => {
+      if (currentValue >= images.length - 1) return 0;
+      return currentValue + 1;
+    });
+  }
+
+  function handlePreviousSlideButtonClick() {
+    setCurrentSlide((currentValue) => {
+      if (currentValue === 0) return images.length - 1;
+      return currentValue - 1;
+    });
+  }
+
+  function renderImages() {
+    return images.map((image, index) => {
+      return (
+        <img
+          key={image}
+          src={image}
+          className={`slide ${index === currentSlide && "active"}`}
+        />
+      );
+    });
+  }
+
+  function renderDots() {
+    return images.map((image, index) => {
+      return (
+        <div
+          key={index}
+          className={`dot ${index === currentSlide && "active"}`}
+        />
+      );
+    });
+  }
+
+  return (
+    <div className="slider">
+      <div className="buttons">
+        <button onClick={handlePreviousSlideButtonClick}>
+          <FaChevronLeft />
+        </button>
+        <button onClick={handleNextSlideButtonClick}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <div className="dots">{renderDots()}</div>
+      <div className="slides">{renderImages()}</div>
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Slider from "../../components/Slider/index";
+
+import sliderBanner from "../../assets/images/sliders/capa.jpg";
+import slide1 from "../../assets/images/sliders/inicial-1.jpg";
+import slide2 from "../../assets/images/sliders/inicial-2.jpg";
+import slide3 from "../../assets/images/sliders/inicial-3.jpg";
 
 const Index = () => {
-  return <div>Index</div>;
+  const sliderImages = [slide1, sliderBanner, slide2, slide3];
+  return (
+    <main>
+      <section>
+        <Slider images={sliderImages} />
+      </section>
+    </main>
+  );
 };
 
 export default Index;

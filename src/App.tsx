863cd7c8a8ac914efa52354be6c3c8d17b8fbc03
Henrique <henriqueferreira.fox@gmail.com>
2023-04-30 20:16:27 -0300

    create scroll button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ScrollButton from "./components/ScrollButton";
 
 const App = () => {
   return (
     <Router>
       <Header />
+      <ScrollButton />
       <Routes>
         <Route index element={<Index />} />
       </Routes>
---
+++ src/components/ScrollButton/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import "./ScrollButton.css";
+
+import ScrollUpButton from "../../assets/images/buttons/up-footer.png";
+
+const ScrollButton = () => {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    window.document.addEventListener("scroll", () => {
+      setActive(window.scrollY > 300);
+    });
+  }, [scrollY]);
+
+  function handleButtonClick() {
+    window.scrollTo(0, 0);
+  }
+  return (
+    <button
+      onClick={handleButtonClick}
+      className={`scroll-btn ${active && "active"}`}
+    >
+      <img src={ScrollUpButton} />
+    </button>
+  );
+};
+
+export default ScrollButton;
